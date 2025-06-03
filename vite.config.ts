77b0e284b5ac686eb6cdd856134cb220fd653ac4
donganh123 <anh.nguyen@dirox.net>
2025-06-03 17:09:14 +0700

    add deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/donganhxauxi.github.io/",
   plugins: [react(), tailwindcss()],
   server: {
     watch: {
