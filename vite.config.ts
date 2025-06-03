41cea6b2742e8a1cc064720aafcee615df3669ae
donganh123 <anh.nguyen@dirox.net>
2025-06-03 17:12:20 +0700

    fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/donganhxauxi.github.io/",
   plugins: [react(), tailwindcss()],
   server: {
     watch: {
